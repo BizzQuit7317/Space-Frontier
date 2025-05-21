46ae2ce286122fb8911e8e08c31d94f8109130d3
BizzQuit7317 <59367989+BizzQuit7317@users.noreply.github.com>
2025-05-21 10:18:39 +0100

    Update main.rs

--- Terrain_Generation/perlin_noise/src/main.rs
+++ Terrain_Generation/perlin_noise/src/main.rs
@@ -1,11 +1,15 @@
 use image::{ImageBuffer, RgbImage, Rgb};
 use noise::{Perlin, NoiseFn};
+use rand::Rng;
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let num: u32 = rng.r#gen();;
+
     let width = 1024;
     let height = 512;
     let scale = 0.01;
-    let seed = 1;
+    let seed = num;
 
     let perlin = Perlin::new(seed);
 
@@ -20,13 +24,15 @@ fn main() {
             let normalized = (value + 1.0) / 2.0;
 
             let pixel = if normalized < 0.3 {
-                Rgb([0, 0, 128])       // Deep water (dark blue)
+                Rgb([5, 102, 141])       // Deep water (dark blue)
             } else if normalized < 0.4 {
-                Rgb([0, 105, 148])     // Shore (light blue)
-            } else if normalized < 0.6 {
-                Rgb([34, 139, 34])     // Grass (green)
+                Rgb([177, 248, 242])     // Shore (light blue)
+            } else if normalized < 0.5 {
+                Rgb([237, 180, 88])     // Shore (Sand)
+            }else if normalized < 0.6 {
+                Rgb([21, 113, 69])     // Grass (green)
             } else if normalized < 0.8 {
-                Rgb([139, 69, 19])     // Mountain (brown)
+                Rgb([8, 7, 8])     // Mountain (brown)
             } else {
                 Rgb([255, 255, 255])   // Snow
             };
